b77bf7008b74819a3374f9ac49144f95d002776c
Edwin Nord <edwin.nord09@gmail.com>
2022-08-20 14:01:47 -0400

    Cats Cards

---
+++ src/Contact.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../src/index.css';
+import phoneIcon from './img/phone-icon.png'
+import emailIcon from './img/email-icon.png'
+
+
+
+
+export default Contact 
+function Contact(props) {
+    return (
+        <div className='card-container'>
+            <div className="contact-card">
+                <img className='cat-name' src={props.img}/>
+                <h3>{props.name}</h3>
+                <div className="info-phone">
+                    <img src={phoneIcon}/>
+                    <p>{props.phone}</p>
+                </div>
+                <div className="info-email">
+                    <img src={emailIcon} />
+                    <p>{props.email}</p>
+                </div>
+            </div>
+
+            {/* <div className="contact-card">
+                    <img className='cat-name' src={fluffykins}/>
+                    <h3>Fluffykins</h3>
+                    <div className="info-group">
+                        <img src={phoneIcon} />
+                        <p>(212) 555-2345</p>
+                    </div>
+                    <div className="info-group">
+                        <img src={emailIcon} />
+                        <p>fluff@me.com</p>
+                    </div>
+            </div>
+                
+                <div className="contact-card">
+                    <img className='cat-name' src={felix}/>
+                    <h3>Felix</h3>
+                    <div className="info-group">
+                        <img src={phoneIcon} />
+                        <p>(212) 555-4567</p>
+                    </div>
+                    <div className="info-group">
+                        <img src={emailIcon} />
+                        <p>thecat@hotmail.com</p>
+                    </div>
+                </div>
+                
+                <div className="contact-card">
+                    <img className='cat-name' src={pumpkin}/>
+                    <h3>Pumpkin</h3>
+                    <div className="info-group">
+                        <img src={phoneIcon} />
+                        <p>(0800) CAT KING</p>
+                    </div>
+                    <div className="info-group">
+                        <img src={emailIcon} />
+                        <p>pumpkin@scrimba.com</p>
+                    </div>
+                </div> */}
+
+        </div>
+        
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import Contact from './Contact';
+
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+function Container() {
+  return (
+    <div className='cards'>
+      <div className='card_one'>
+        <Contact
+          img = "./img/cat1.jpg"
+          name = "Mr. Whiskerson"
+          phone = "(212) 555-1234"
+          email = "mr.whiskaz@catnap.meow"
+        />
+      </div>
+
+      <div className='card_two'>
+        <Contact
+          img = "./img/cat2.jpg"
+          name = "Fluffykins"
+          phone = "(212) 555-2345"
+          email = "fluff@me.com"
+        />
+      </div>
+
+      <div className='card_three'>
+        <Contact
+          img = "./img/cat3.jpg"
+          name = "Felix"
+          phone = "(212) 555-4567"
+          email = "thecat@hotmail.com"
+        />
+      </div>
+
+      <div className='card_four'>
+        <Contact
+          img = "./img/cat4.jpg"
+          name = "Pumpkin"
+          phone = "(0800) CAT KING"
+          email = "pumpkin@scrimba.com"
+        />
+      </div>
+
+    </div>
+  )
+} 
+
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Container/>)
+  
